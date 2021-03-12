67c490d6751d63977980e8937a5773af311c47ed
maximzubkov <zubkov.md@phystech.edu>
2021-03-12 12:13:00 +0300

    Add code2class

--- models/encoders/__init__.py
+++ models/encoders/__init__.py
@@ -1,12 +1,13 @@
+from .code2class import Code2ClassModel
 from .lstm import LSTMModel
-from code2seq.model import Code2Class
 
 __all__ = [
     "LSTMModel",
+    "Code2ClassModel",
     "encoder_models"
 ]
 
 encoder_models = {
     "LSTM": LSTMModel,
-    "Code2Class": Code2Class
+    "Code2Class": Code2ClassModel
 }
---
+++ models/encoders/code2class.py
@@ -0,0 +1,15 @@
+from code2seq.dataset import PathContextBatch
+from code2seq.model import Code2Class
+from code2seq.utils.vocabulary import Vocabulary
+from omegaconf import DictConfig
+from torch import nn
+
+
+class Code2ClassModel(nn.Module):
+    def __init__(self, config: DictConfig, vocabulary: Vocabulary):
+        super().__init__()
+        self.num_classes = config.num_classes
+        self.code2class = Code2Class(config, vocabulary)
+
+    def forward(self, batch: PathContextBatch):
+        return self.code2class(batch.contexts, batch.contexts_per_label)

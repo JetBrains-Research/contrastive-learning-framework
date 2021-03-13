937c85f671c15135b7d7fbf883012c7a1cb35553
maximzubkov <zubkov.md@phystech.edu>
2021-03-13 12:18:26 +0300

    Switch to CIFAR in tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,14 +1,15 @@
-from torchvision.datasets import MNIST
+from torchvision.datasets import CIFAR10
 
 from dataset.contrastive_dataset import ContrastiveDataset
 
 
 def test_contrastive_dataset():
-    clf_dataset = MNIST(root="./data", train=False, download=True)
+    clf_dataset = CIFAR10(root="./data", train=False, download=True)
     contr_dataset = ContrastiveDataset(clf_dataset=clf_dataset)
-    items = contr_dataset.idx2pair.items()
-    for i in range(200):
-        _, (a_idx, b_idx) = next(items)
+    for i, item in enumerate(contr_dataset.idx2pair.items()):
+        if i == 200:
+            break
+        _, (a_idx, b_idx) = item
         _, label_a = clf_dataset[a_idx]
         _, label_b = clf_dataset[b_idx]
         assert label_a == label_b

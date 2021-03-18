81aafadce365f6910db49b3b685e7640742a1888
maximzubkov <zubkov.md@phystech.edu>
2021-03-18 21:50:06 +0300

    Add new configs to models

--- models/encoders/lstm.py
+++ models/encoders/lstm.py
@@ -1,26 +1,25 @@
 import torch
 import torch.nn as nn
-
-from configs import LSTMConfig
+from omegaconf import DictConfig
 
 
 class LSTMModel(nn.Module):
-    def __init__(self, config: LSTMConfig):
+    def __init__(self, config: DictConfig):
         super().__init__()
         self.num_classes = config.num_classes
-        self.hidden_size = config.hidden_size
-        self.dropout = nn.Dropout(config.dropout)
+        self.hidden_size = config.encoder.hidden_size
+        self.dropout = nn.Dropout(config.encoder.dropout)
         self.embeddings = nn.Embedding(
-            config.vocab_size,
-            config.embedding_size,
-            padding_idx=config.pad_id,
+            config.dataset.vocab_size,
+            config.encoder.embedding_size,
+            padding_idx=config.dataset.pad_id,
         )
         self.lstm = nn.LSTM(
-            input_size=config.embedding_size,
-            hidden_size=config.hidden_size,
-            bidirectional=config.bidirectional
+            input_size=config.encoder.embedding_size,
+            hidden_size=config.encoder.hidden_size,
+            bidirectional=config.encoder.bidirectional
         )
-        self.fc = nn.Linear(config.hidden_size, config.num_classes)
+        self.fc = nn.Linear(config.encoder.hidden_size, config.num_classes)
 
     def forward(self, seq: torch.Tensor):
         out = self.embeddings(seq)

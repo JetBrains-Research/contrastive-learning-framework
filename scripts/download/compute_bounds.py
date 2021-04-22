5d71dbad1a1bbc993d7acaad91237a657284dc26
maximzubkov <zubkov.md@phystech.edu>
2021-04-22 21:44:13 +0300

    Update splitting

---
+++ scripts/download/compute_bounds.py
@@ -0,0 +1,50 @@
+from argparse import ArgumentParser
+from os import listdir
+from os.path import join, isdir
+import sys
+from collections import defaultdict
+
+
+def get_task(clones_dir: str):
+    parts = clones_dir.split("_")
+    return f"{parts[-6]}{parts[-5]}"
+
+
+def compute_bounds(
+        data_path: str,
+        train_part: float,
+        test_part: float,
+):
+    sum_ = 0
+    task2count = defaultdict(int)
+    for clones_dir in listdir(data_path):
+        clones_dir_path = join(data_path, clones_dir)
+        if isdir(clones_dir_path):
+            task2count[get_task(clones_dir)] += 1
+            sum_ += 1
+
+    count = 0
+    train_bound, test_bound = None, None
+
+    for k in sorted(task2count):
+        count += task2count[k]
+        if (100 * (count / sum_) > train_part) and (train_bound is None):
+            train_bound = count
+        if (100 * (count / sum_) > train_part + test_part) and (test_bound is None):
+            test_bound = count
+    return train_bound, test_bound
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("data_path", type=str)
+    arg_parser.add_argument("train_part", type=float, default=60)
+    arg_parser.add_argument("test_part", type=float, default=20)
+    args = arg_parser.parse_args()
+
+    train_bound_, test_bound_ = compute_bounds(
+        data_path=args.data_path,
+        train_part=args.train_part,
+        test_part=args.test_part,
+    )
+    print(f"{train_bound_} {test_bound_}")
--- scripts/download/split_dataset.sh
+++ scripts/download/split_dataset.sh
@@ -3,19 +3,22 @@
 # Default values
 # options:
 # -h, --help      show brief help
-# $1              specify a directory where dataset is located
-# $2              specify a directory to store output in
-# $3              specify a percentage of dataset used as train set
-# $4              specify a percentage of dataset used as test set
-# $5              specify a percentage of dataset used as validation set
+# $1              specify a dataset name
+# $2              specify a directory where dataset is located
+# $3              specify a directory to store output in
+# $4              specify a percentage of dataset used as train set
+# $5              specify a percentage of dataset used as test set
+# $6              specify a percentage of dataset used as validation set
 
 SHUFFLE=false
 
-ORIGINAL_DATASET_PATH=$1
-SPLIT_DATASET_PATH=$2
-TRAIN_SPLIT_PART=$3
-TEST_SPLIT_PART=$4
-VAL_SPLIT_PART=$5
+DATASET_NAME=$1
+ORIGINAL_DATASET_PATH=$2
+SPLIT_DATASET_PATH=$3
+TRAIN_SPLIT_PART=$4
+TEST_SPLIT_PART=$5
+VAL_SPLIT_PART=$6
+COMPUTE_BOUNDS_SCRIPT=scripts/download/compute_bounds.py
 
 DIR_TRAIN="${SPLIT_DATASET_PATH}/train"
 DIR_VAL="${SPLIT_DATASET_PATH}/val"
@@ -41,29 +44,43 @@ mkdir "$DIR_TEST"
 
 cp -r "$ORIGINAL_DATASET_PATH"/* "$DIR_TRAIN"/
 
-find "$DIR_TRAIN"/* -type d -exec basename {} \; | while read DIR_CLASS
+num_files=$(find "$DIR_TRAIN" -mindepth 1 -type d | wc -l)
+
+if [ $DATASET_NAME == "codeforces" ]
+then
+  basenames=$(find "$DIR_TRAIN" -mindepth 1 -type d | \
+              awk -F '_' '{print $(NF-5)$(NF-4), $0}' | \
+              sort -k1 | \
+              cut -d ' ' -f2-)
+  bounds=$(python $COMPUTE_BOUNDS_SCRIPT "$DIR_TRAIN" "$TRAIN_SPLIT_PART" "$TEST_SPLIT_PART")
+  train_bound=$(echo $bounds | cut -d " " -f1)
+  test_bound=$(echo $bounds | cut -d " " -f2)
+  echo $train_bound
+elif [ $DATASET_NAME == "poj_104" ]
+then
+  basenames=$(find "$DIR_TRAIN"/* -type d)
+  train_bound=$(expr $num_files \* $TRAIN_SPLIT_PART / 100)
+  test_bound=$(expr $train_bound + $num_files \* $TEST_SPLIT_PART / 100)
+else
+  echo "Dataset $DATASET_NAME does not exist"
+fi
+
+counter=$(expr 0)
+
+for DIR_CLASS in $basenames
 do
-    echo "Splitting class - $DIR_CLASS";
-    mkdir "$DIR_VAL/$DIR_CLASS"
-    mkdir "$DIR_TEST/$DIR_CLASS"
-    num_files=$(find "$DIR_TRAIN/$DIR_CLASS" -type f | wc -l)
-    train_bound=$(expr $num_files \* $TRAIN_SPLIT_PART / 100)
-    test_bound=$(expr $train_bound + $num_files \* $TEST_SPLIT_PART / 100)
+    echo "Splitting class - $DIR_CLASS"
 
-    counter=$(expr 0)
+    counter=$(expr $counter + 1)
+    if [[ $counter -gt $train_bound ]] && [[ $counter -le $test_bound ]];
+    then
+        mv "$DIR_CLASS" "$DIR_TEST"
+    fi
 
-    files=$(find "$DIR_TRAIN/$DIR_CLASS" -type f -exec basename {} \;)
-
-    for file in $files;
-    do
-        counter=$(expr $counter + 1)
-        if [ $counter -gt $train_bound ] && [ $counter -le $test_bound ]; then
-            mv "$DIR_TRAIN/$DIR_CLASS/$file" "$DIR_TEST/$DIR_CLASS/$file"
-        fi
-        if [ $counter -gt $test_bound ]; then
-            mv "$DIR_TRAIN/$DIR_CLASS/$file" "$DIR_VAL/$DIR_CLASS/$file"
-        fi
-    done
+    if [[ $counter -gt $test_bound ]];
+    then
+        mv "$DIR_CLASS" "$DIR_VAL"
+    fi
 done
 
 echo "Done"

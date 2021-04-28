f439b97b1878e3532c8f3ccef7cd7189da0ab215
maximzubkov <zubkov.md@phystech.edu>
2021-04-28 15:33:03 +0300

    Add switch from cpu torch-geometric to cuda

--- scripts/install_torch_geometric.sh
+++ scripts/install_torch_geometric.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Script - installing torch_geometric, a framework for working with graph data in pytorch
 
-# TODO: switch to GPU if necessary
-CUDA=cpu
 TORCH=1.7.1
+CUDA_AVAILABLE=$(python -c 'import torch; print(torch.cuda.is_available())')
+
+if [ $CUDA_AVAILABLE == "True" ]
+then
+    CUDA=cu102
+else
+    CUDA=cpu
+fi
 
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html

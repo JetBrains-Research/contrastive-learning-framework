0797ea52445321b109fd501b61aeb23dd0499fa6
maximzubkov <zubkov.md@phystech.edu>
2021-05-09 16:38:08 +0300

    Add roc auc computing to SimCLR

--- models/self_supervised/simclr.py
+++ models/self_supervised/simclr.py
@@ -4,7 +4,8 @@ from os.path import join, isdir
 import torch
 from code2seq.utils.vocabulary import Vocabulary
 from omegaconf import DictConfig
-from pl_bolts.metrics import mean, precision_at_k
+from pl_bolts.metrics import mean
+from pytorch_lightning.metrics.functional import auroc
 from pl_bolts.models.self_supervised import SimCLR
 
 from models.encoders import encoder_models
@@ -29,8 +30,8 @@ class SimCLRModel(SimCLR):
         for class_ in listdir(train_data_path):
             class_path = join(train_data_path, class_)
             if isdir(class_path):
-                num_samples += len([_ for _ in listdir(class_path)])
-
+                num_files = len([_ for _ in listdir(class_path)])
+                num_samples += num_files * (num_files - 1) // 2
         super().__init__(
             gpus=-1,
             num_samples=num_samples,
@@ -84,35 +85,69 @@ class SimCLRModel(SimCLR):
         z1 = self.projection(h1)
         z2 = self.projection(h2)
 
-        loss = self.nt_xent_loss(z1, z2, self.temperature)
+        if y is None:
+            mask = torch.eye(z1.shape[0], dtype=torch.float32, device=z1.device)
+        else:
+            y = y.contiguous().view(-1, 1)
+            mask = torch.eq(y, y.T).float()
+
+        contrast_feature = torch.cat([z1, z2], dim=0)
+        logits = torch.matmul(contrast_feature, contrast_feature.T)
+        mask = mask.repeat(2, 2)
+
+        loss = self._loss(logits, mask)
 
         with torch.no_grad():
-            logits = torch.mm(z1, z2.T)
-            labels = torch.arange(logits.shape[0], dtype=torch.long)
-            labels = labels.type_as(logits)
-            acc1, acc5 = precision_at_k(logits, labels, top_k=(1, 5))
+            roc_auc = auroc(logits.reshape(-1), mask.reshape(-1))
 
-        return loss, acc1, acc5
+        return loss, roc_auc
+
+    def _loss(self, logits, mask):
+        batch_size = mask.shape[0] // 2
+
+        # compute logits
+        anchor_dot_contrast = logits / self.temperature
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(2 * batch_size).view(-1, 1),
+            0
+        )
+        mask_ = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask_ * log_prob).sum(1) / mask_.sum(1)
+        loss = -mean_log_prob_pos.view(2, batch_size).mean()
+
+        return loss
 
     def training_step(self, batch, batch_idx):
-        loss, acc1, acc5 = self.shared_step(batch)
+        loss, roc_auc = self.shared_step(batch)
 
-        log = {'train_loss': loss, 'train_acc1': acc1, 'train_acc5': acc5}
+        log = {'train_loss': loss, 'train_roc_auc': roc_auc}
         self.log_dict(log)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, acc1, acc5 = self.shared_step(batch)
+        loss, roc_auc = self.shared_step(batch)
 
-        logs = {'val_loss': loss, 'val_acc1': acc1, 'val_acc5': acc5}
+        logs = {'val_loss': loss, 'val_roc_auc': roc_auc}
         return logs
 
     def validation_epoch_end(self, outputs):
         val_loss = mean(outputs, 'val_loss')
-        val_acc1 = mean(outputs, 'val_acc1')
-        val_acc5 = mean(outputs, 'val_acc5')
+        val_roc_auc = mean(outputs, 'val_roc_auc')
 
-        log = {'val_loss': val_loss, 'val_acc1': val_acc1, 'val_acc5': val_acc5}
+        log = {'val_loss': val_loss, 'val_roc_auc': val_roc_auc}
         self.log_dict(log)
 
 

8fe0db02c02854e43dac281f793cd096c0636ece
maximzubkov <zubkov.md@phystech.edu>
2021-05-09 17:01:31 +0300

    Call PathEncoder and PathClassifier separately

--- models/encoders/code2class.py
+++ models/encoders/code2class.py
@@ -1,6 +1,6 @@
 from code2seq.dataset import PathContextBatch
-from code2seq.model import Code2Class
-from code2seq.utils.vocabulary import Vocabulary
+from code2seq.model.modules import PathEncoder, PathClassifier
+from code2seq.utils.vocabulary import Vocabulary, PAD
 from omegaconf import DictConfig
 from torch import nn
 
@@ -9,7 +9,15 @@ class Code2ClassModel(nn.Module):
     def __init__(self, config: DictConfig, vocabulary: Vocabulary):
         super().__init__()
         self.num_classes = config.num_classes
-        self.code2class = Code2Class(config, vocabulary)
+        self.encoder = PathEncoder(
+            config.encoder,
+            config.classifier.classifier_input_size,
+            len(vocabulary.token_to_id),
+            vocabulary.token_to_id[PAD],
+            len(vocabulary.node_to_id),
+            vocabulary.node_to_id[PAD],
+        )
+        self.classifier = PathClassifier(config.classifier, config.num_classes)
 
     def forward(self, batch: PathContextBatch):
-        return self.code2class(batch.contexts, batch.contexts_per_label)
+        return self.classifier(self.encoder(batch.contexts), batch.contexts_per_label)

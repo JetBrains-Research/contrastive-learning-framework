9cddbd1556606230e5cbf58c938441160a70c23a
maximzubkov <zubkov.md@phystech.edu>
2021-05-23 20:13:46 +0300

    Add tests for map at k

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,7 +2,7 @@ import torch
 from torchmetrics.functional import confusion_matrix
 
 import numpy
-from models.self_supervised.utils import compute_f1
+from models.self_supervised.utils import compute_f1, compute_map_at_k
 from sklearn.metrics import f1_score
 
 
@@ -13,3 +13,18 @@ def test_f1():
     conf_matrix = confusion_matrix(torch.LongTensor(preds), torch.LongTensor(target), num_classes=2)
     custom_f1 = compute_f1(conf_matrix)
     assert numpy.isclose(custom_f1, sklearn_f1)
+
+
+def test_map_at_k():
+    preds = torch.Tensor(
+        [
+            [True, False, False, False],
+            [False, True, False, False],
+            [False, False, True, False],
+            [False, False, False, True],
+            [True, False, True, False],
+            [True, True, True, True],
+        ]
+    )
+    map_at_k = compute_map_at_k(preds)
+    assert numpy.isclose(map_at_k * 6, (1 + 1/2 + 1/3 + 1/4 + 0.5 * (1 + 2/3) + 1))

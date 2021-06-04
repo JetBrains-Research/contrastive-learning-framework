b5de8a874a37410b83355f9553b1f51f6cce1227
maximzubkov <zubkov.md@phystech.edu>
2021-06-04 21:04:33 +0300

    Update utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
-from .utils import is_c_family_file, is_json_file
+from .utils import is_c_family_file, is_json_file, get_task
 
 __all__ = [
     "is_c_family_file",
-    "is_json_file"
+    "is_json_file",
+    "get_task"
 ]
--- utils/utils.py
+++ utils/utils.py
@@ -11,3 +11,8 @@ def is_c_family_file(path: str):
 def is_json_file(path: str):
     ext = path.rsplit(".", 1)[-1]
     return isfile(path) and (ext == "json")
+
+
+def get_task(label: str):
+    parts = label.split("_")
+    return f"{parts[-6]}{parts[-5]}"

5548ba08a70d86c0ffa219bdee6cce0519c72ea8
maximzubkov <zubkov.md@phystech.edu>
2021-08-03 20:39:54 +0300

    Scripts and configs for code-transfromer

--- scripts/install_code_transformer.sh
+++ scripts/install_code_transformer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+BUILD_DIR=build
+CODE_TRANSFORMER=code-transformer
 CONFIG_PATH=~/.config
-CONFIGS_DIR="$CONFIG_PATH"/code_transformer
+CONFIGS_DIR="$CONFIG_PATH"/"$CODE_TRANSFORMER"
 ENV_FILE_PATH="$CONFIGS_DIR"/.env
 
 if [ ! -d "$CONFIG_PATH" ]
@@ -14,4 +16,8 @@ then
     mkdir "$CONFIGS_DIR"
 fi
 
-cp configs/code-transformer.env "$ENV_FILE_PATH"
+cp configs/code-transformer/.env "$ENV_FILE_PATH"
+
+git clone https://github.com/maximzubkov/code-transformer.git "$BUILD_DIR"/"$CODE_TRANSFORMER"
+cd "$BUILD_DIR"/"$CODE_TRANSFORMER" && git checkout cpp-dataset && pip install -e .
+pip install git+https://github.com/tree-sitter/py-tree-sitter.git

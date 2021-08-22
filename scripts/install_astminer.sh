3e266a91c12ba13ac4c96b621ea2617e2f447313
maximzubkov <zubkov.md@phystech.edu>
2021-08-22 14:30:51 +0300

    Update building scripts

--- scripts/install_astminer.sh
+++ scripts/install_astminer.sh
@@ -4,6 +4,7 @@
 
 BUILD_DIR=build
 ASTMINER=astminer
+COMMIT_HASH=c1e35b0a3d4d4ee4f6f13ac3d52a876313ce2cf7
 
 git clone https://github.com/JetBrains-Research/astminer.git $BUILD_DIR/"$ASTMINER"
-cd $BUILD_DIR/"$ASTMINER" && ./gradlew shadowJar
+cd $BUILD_DIR/"$ASTMINER" && git checkout "$COMMIT_HASH" && ./gradlew shadowJar

282e982010c05ecc641e5c93529797ec509e214d
maximzubkov <zubkov.md@phystech.edu>
2021-08-25 15:28:59 +0300

    Fix swav

--- models/self_supervised/swav.py
+++ models/self_supervised/swav.py
@@ -2,15 +2,15 @@ from os.path import join
 
 import numpy as np
 import torch
-from code2seq.data.vocabulary import Vocabulary
+import torch.nn.functional as F
 from omegaconf import DictConfig
 from pl_bolts.models.self_supervised import SwAV
 from pl_bolts.models.self_supervised.swav.swav_resnet import MultiPrototypes
+from torch import nn
 
-from models.encoders import encoder_models
 from models.self_supervised.utils import (
     validation_metrics,
-    compute_num_samples
+    compute_num_samples, init_model, prepare_features, roc_auc
 )
 
 
@@ -35,14 +35,12 @@ class SwAVModel(SwAV):
             gpus=config.ssl.gpus,
             num_samples=num_samples,
             batch_size=config.hyper_parameters.batch_size,
-            epoch_queue_starts=config.ssl.epoch_queue_starts,
             max_epochs=config.ssl.max_epochs,
             freeze_prototypes_epochs=config.ssl.freeze_prototypes_epochs,
             epsilon=config.ssl.epsilon,
             hidden_mlp=config.ssl.hidden_mlp,
             feat_dim=config.ssl.feat_dim,
             dataset="",
-            nmb_crops=config.ssl.nmb_crops,
             nmb_prototypes=config.ssl.nmb_prototypes
         )
 
@@ -52,63 +50,49 @@ class SwAVModel(SwAV):
         elif config.ssl.nmb_prototypes > 0:
             self.prototypes = torch.nn.Linear(config.ssl.feat_dim, config.ssl.nmb_prototypes, bias=False)
 
-    def init_model(self):
-        if self.base_encoder == "transformer":
-            encoder = encoder_models[self.base_encoder](self.config)
-        elif self.base_encoder == "code2class":
-            _vocabulary = Vocabulary(
-                join(
-                    self.config.data_folder,
-                    self.config.dataset.name,
-                    self.config.dataset.dir,
-                    self.config.vocabulary_name
-                ),
-                self.config.dataset.max_labels,
-                self.config.dataset.max_tokens
-            )
-            encoder = encoder_models[self.base_encoder](config=self.config, vocabulary=_vocabulary)
-        elif self.config.name == "gnn":
-            encoder = encoder_models[self.config.name](self.config)
-        elif self.config.name == "code-transformer":
-            encoder = encoder_models[self.config.name](self.config)
-        else:
-            raise ValueError(f"Unknown model: {self.config.name}")
+        # normalize output features
+        self.l2norm = config.ssl.normalize
 
+        # projection head
+        self.projection_head = nn.Sequential(
+            nn.Linear(config.ssl.feat_dim, config.ssl.hidden_mlp),
+            nn.BatchNorm1d(config.ssl.hidden_mlp),
+            nn.ReLU(inplace=True),
+            nn.Linear(config.ssl.hidden_mlp, config.num_classes),
+        )
+
+    def init_model(self):
+        encoder = init_model(self.config)
         return encoder
 
     def forward(self, x):
         q = self.model(x)
-        q = torch.nn.functional.normalize(q, dim=1)
+        q = F.normalize(q, dim=1)
         return q
 
-    def _loss(self, embedding, output, bs):
-        # 3. swav loss computation
-        loss = 0
-        for i, crop_id in enumerate(self.crops_for_assign):
-            with torch.no_grad():
-                out = output[bs * crop_id:bs * (crop_id + 1)]
+    def _loss(self, queries, keys, labels):
+        embedding, loss_labels = prepare_features(queries, keys, labels)
+        embedding = self.projection_head(embedding)
+        if self.l2norm:
+            embedding = F.normalize(embedding, dim=1, p=2)
+        prototype = self.prototypes(embedding)
+        bs = labels.shape[0]
+        # prototype output: 2BxK
+        scores_t = prototype[:bs]
+        scores_s = prototype[bs:]
 
-                # 4. time to use the queue
-                if self.queue is not None:
-                    if self.use_the_queue or not torch.all(self.queue[i, -1, :] == 0):
-                        self.use_the_queue = True
-                        out = torch.cat((torch.mm(self.queue[i], self.prototypes.weight.t()), out))
-                    # fill the queue
-                    self.queue[i, bs:] = self.queue[i, :-bs].clone()
-                    self.queue[i, :bs] = embedding[crop_id * bs:(crop_id + 1) * bs]
+        q_t = torch.exp(scores_t / self.epsilon).t()
+        q_s = torch.exp(scores_s / self.epsilon).t()
 
-                # 5. get assignments
-                q = torch.exp(out / self.epsilon).t()
-                q = self.get_assignments(q, self.sinkhorn_iterations)[-bs:]
+        with torch.no_grad():
+            q_t = self.sinkhorn(q_t, self.sinkhorn_iterations)
+            q_s = self.sinkhorn(q_s, self.sinkhorn_iterations)
 
-            # cluster assignment prediction
-            subloss = 0
+        # convert scores to probabilities
+        p_t = self.softmax(scores_t / self.temperature)
+        p_s = self.softmax(scores_s / self.temperature)
 
-            for v in np.delete(np.arange(np.sum(self.nmb_crops)), crop_id):
-                p = self.softmax(output[bs * v:bs * (v + 1)] / self.temperature)
-                subloss -= torch.mean(torch.sum(q * torch.log(p), dim=1))
-            loss += subloss / (np.sum(self.nmb_crops) - 1)
-        loss /= len(self.crops_for_assign)
+        loss = -0.5 * torch.mean(q_t * torch.log(p_s) + q_s * torch.log(p_t))
 
         return loss
 
@@ -118,22 +102,20 @@ class SwAVModel(SwAV):
         # 1. normalize the prototypes
         with torch.no_grad():
             w = self.prototypes.weight.data.clone()
-            w = torch.nn.functional.normalize(w, dim=1, p=2)
+            w = F.normalize(w, dim=1, p=2)
             self.prototypes.weight.copy_(w)
 
         # 2. multi-res forward passes
-        embedding = torch.cat((self.model(q), self.model(k)))
-        output = self.prototypes(embedding)
-        embedding = embedding.detach()
+        queries, keys = self.model(q), self.model(k)
+        loss = self._loss(queries=queries, keys=keys, labels=labels)
 
-        loss = self._loss(embedding, output, bs=labels.shape[0])
-
-        self.log('train_loss', loss, on_step=True, on_epoch=False)
+        roc_auc_ = roc_auc(queries=queries, keys=keys, labels=labels)
+        self.log_dict({"train_loss": loss, "train_roc_auc": roc_auc_})
         return loss
 
     def validation_step(self, batch, batch_idx):
-        inputs, labels = batch
-        features = self(inputs)
+        features, labels = batch
+        features = self(features)
         labels = labels.contiguous().view(-1, 1)
 
         return {"features": features, "labels": labels}

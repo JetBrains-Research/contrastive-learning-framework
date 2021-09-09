1e3f074bd0fe47c89681676591323f1d37616576
maximzubkov <zubkov.md@phystech.edu>
2021-09-09 16:03:23 +0300

    Fix eval setup

--- eval_infercode.py
+++ eval_infercode.py
@@ -12,22 +12,22 @@ data_dir = "data"
 def compute_metrics(dataset):
     storage_path = join(data_dir, dataset, "infercode")
     labels, vectors = [], []
-    for i, file in listdir(storage_path):
+    for file in listdir(storage_path):
         file_path = join(storage_path, file)
         with open(file_path, "rb") as f:
             data = pickle.load(f)
         labels += [basename(dirname(key)) for key in data]
-        vectors += [torch.Tensor(data[key]) for key in data]
+        vectors += [torch.Tensor(data[key]).reshape(1, -1) for key in data]
     labels_set = set(labels)
     label2id = {label: i for i, label in enumerate(labels_set)}
     labels = torch.LongTensor([label2id[label] for label in labels])
-    features = torch.cat(vectors)
-    log = validation_metrics({"features": features, "labels": labels}, task=dataset)
+    features = torch.cat(vectors, dim=0)
+    log = validation_metrics([{"features": features, "labels": labels}], task=dataset)
     print(log)
 
 
 if __name__ == "__main__":
     print("Codeforces:")
-    compute_metrics("codeforces", ks=(5, 10, 15))
+    compute_metrics("codeforces")
     print("POJ 104:")
-    compute_metrics("poj_104", ks=(100, 200, 500))
+    compute_metrics("poj_104")

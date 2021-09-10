1813e7324038602e8a6c29b86028cff319df4151
maximzubkov <zubkov.md@phystech.edu>
2021-09-10 12:26:30 +0300

    Update script

--- trans_coder/run_transcoder.py
+++ trans_coder/run_transcoder.py
@@ -10,21 +10,11 @@
 #
 
 
-import os
 import argparse
+import os
 from pathlib import Path
-import sys
-import fastBPE
+
 import torch
-from codegen_sources.model.src.logger import create_logger
-from codegen_sources.preprocessing.lang_processors.cpp_processor import CppProcessor
-from codegen_sources.preprocessing.lang_processors.java_processor import JavaProcessor
-from codegen_sources.preprocessing.lang_processors.python_processor import (
-    PythonProcessor,
-)
-from codegen_sources.preprocessing.lang_processors.lang_processor import LangProcessor
-from codegen_sources.preprocessing.bpe_modes.fast_bpe_mode import FastBPEMode
-from codegen_sources.preprocessing.bpe_modes.roberta_bpe_mode import RobertaBPEMode
 from codegen_sources.model.src.data.dictionary import (
     Dictionary,
     BOS_WORD,
@@ -33,9 +23,12 @@ from codegen_sources.model.src.data.dictionary import (
     UNK_WORD,
     MASK_WORD,
 )
-from codegen_sources.model.src.utils import restore_roberta_segmentation_sentence
+from codegen_sources.model.src.logger import create_logger
 from codegen_sources.model.src.model import build_model
 from codegen_sources.model.src.utils import AttrDict
+from codegen_sources.preprocessing.bpe_modes.fast_bpe_mode import FastBPEMode
+from codegen_sources.preprocessing.bpe_modes.roberta_bpe_mode import RobertaBPEMode
+from codegen_sources.preprocessing.lang_processors.lang_processor import LangProcessor
 
 SUPPORTED_LANGUAGES = ["cpp", "java", "python"]
 
@@ -51,37 +44,13 @@ def get_parser():
 
     # model
     parser.add_argument("--model_path", type=str, default="", help="Model path")
-    parser.add_argument(
-        "--src_lang",
-        type=str,
-        default="",
-        help=f"Source language, should be either {', '.join(SUPPORTED_LANGUAGES[:-1])} or {SUPPORTED_LANGUAGES[-1]}",
-    )
-    parser.add_argument(
-        "--tgt_lang",
-        type=str,
-        default="",
-        help=f"Target language, should be either {', '.join(SUPPORTED_LANGUAGES[:-1])} or {SUPPORTED_LANGUAGES[-1]}",
-    )
-    parser.add_argument(
-        "--BPE_path",
-        type=str,
-        default=str(
-            Path(__file__).parents[2].joinpath("data/bpe/cpp-java-python/codes")
-        ),
-        help="Path to BPE codes.",
-    )
-    parser.add_argument(
-        "--beam_size",
-        type=int,
-        default=1,
-        help="Beam size. The beams will be printed in order of decreasing likelihood.",
-    )
+    parser.add_argument("--BPE_path", type=str, default="", help="Path to BPE codes.")
+    parser.add_argument("--code_path", type=str, default=None, help="Path to the program file")
 
     return parser
 
 
-class Translator:
+class Encoder:
     def __init__(self, model_path, BPE_path):
         # reload model
         reloaded = torch.load(model_path, map_location="cpu")
@@ -122,98 +91,42 @@ class Translator:
 
     def embed(
         self,
-        input,
+        input_,
         lang="cpp",
-        suffix1="_sa",
-        suffix2="_sa",
-        n=1,
-        sample_temperature=None,
+        suffix="_sa",
         device="cuda:0",
     ):
-        src_lang_processor = LangProcessor.processors[lang1](
+        src_lang_processor = LangProcessor.processors[lang](
             root_folder=Path(__file__).parents[2].joinpath("tree-sitter")
         )
         tokenizer = src_lang_processor.tokenize_code
-        tgt_lang_processor = LangProcessor.processors[lang2](
-            root_folder=Path(__file__).parents[2].joinpath("tree-sitter")
-        )
-        detokenizer = tgt_lang_processor.detokenize_code
 
-        lang1 += suffix1
-        lang2 += suffix2
+        lang += suffix
 
         assert (
-            lang1 in self.reloaded_params.lang2id.keys()
-        ), f"{lang1} should be in {self.reloaded_params.lang2id.keys()}"
-        assert (
-            lang2 in self.reloaded_params.lang2id.keys()
-        ), f"{lang2} should be in {self.reloaded_params.lang2id.keys()}"
+            lang in self.reloaded_params.lang2id.keys()
+        ), f"{lang} should be in {self.reloaded_params.lang2id.keys()}"
 
         with torch.no_grad():
-
-            lang1_id = self.reloaded_params.lang2id[lang1]
-            lang2_id = self.reloaded_params.lang2id[lang2]
+            lang_id = self.reloaded_params.lang2id[lang]
 
             # Convert source code to ids
-            tokens = [t for t in tokenizer(input)]
-            print(f"Tokenized {params.src_lang} function:")
-            print(tokens)
+            tokens = [t for t in tokenizer(input_)]
             tokens = self.bpe_model.apply_bpe(" ".join(tokens)).split()
             tokens = ["</s>"] + tokens + ["</s>"]
-            input = " ".join(tokens)
+            input_ = " ".join(tokens)
 
             # Create torch batch
-            len1 = len(input.split())
-            len1 = torch.LongTensor(1).fill_(len1).to(device)
-            x1 = torch.LongTensor([self.dico.index(w) for w in input.split()]).to(
-                device
-            )[:, None]
-            langs1 = x1.clone().fill_(lang1_id)
+            len_ = len(input_.split())
+            len_ = torch.LongTensor(1).fill_(len_).to(device)
+            x = torch.LongTensor([self.dico.index(w) for w in input_.split()]).to(device)[:, None]
+            langs = x.clone().fill_(lang_id)
 
             # Encode
-            enc1 = self.encoder("fwd", x=x1, lengths=len1, langs=langs1, causal=False)
-            enc1 = enc1.transpose(0, 1)
-            if n > 1:
-                enc1 = enc1.repeat(n, 1, 1)
-                len1 = len1.expand(n)
+            enc_ = self.encoder("fwd", x=x, lengths=len_, langs=langs, causal=False)
+            enc_ = enc_.transpose(0, 1)
 
-            # Decode
-            if beam_size == 1:
-                x2, len2 = self.decoder.generate(
-                    enc1,
-                    len1,
-                    lang2_id,
-                    max_len=int(
-                        min(self.reloaded_params.max_len, 3 * len1.max().item() + 10)
-                    ),
-                    sample_temperature=sample_temperature,
-                )
-            else:
-                x2, len2, _ = self.decoder.generate_beam(
-                    enc1,
-                    len1,
-                    lang2_id,
-                    max_len=int(
-                        min(self.reloaded_params.max_len, 3 * len1.max().item() + 10)
-                    ),
-                    early_stopping=False,
-                    length_penalty=1.0,
-                    beam_size=beam_size,
-                )
-
-            # Convert out ids to text
-            tok = []
-            for i in range(x2.shape[1]):
-                wid = [self.dico[x2[j, i].item()] for j in range(len(x2))][1:]
-                wid = wid[: wid.index(EOS_WORD)] if EOS_WORD in wid else wid
-                if getattr(self.reloaded_params, "roberta_mode", False):
-                    tok.append(restore_roberta_segmentation_sentence(" ".join(wid)))
-                else:
-                    tok.append(" ".join(wid).replace("@@ ", ""))
-            results = []
-            for t in tok:
-                results.append(detokenizer(t))
-            return results
+            return enc_
 
 
 if __name__ == "__main__":
@@ -221,27 +134,15 @@ if __name__ == "__main__":
     parser = get_parser()
     params = parser.parse_args()
 
-    # check parameters
-    assert os.path.isfile(
-        params.model_path
-    ), f"The path to the model checkpoint is incorrect: {params.model_path}"
-    assert os.path.isfile(
-        params.BPE_path
-    ), f"The path to the BPE tokens is incorrect: {params.BPE_path}"
-    assert (
-        params.src_lang in SUPPORTED_LANGUAGES
-    ), f"The source language should be in {SUPPORTED_LANGUAGES}."
-    assert (
-        params.tgt_lang in SUPPORTED_LANGUAGES
-    ), f"The target language should be in {SUPPORTED_LANGUAGES}."
+    assert os.path.isfile(params.model_path), f"The path to the model checkpoint is incorrect: {params.model_path}"
+    assert os.path.isfile(params.BPE_path), f"The path to the BPE tokens is incorrect: {params.BPE_path}"
+    assert os.path.isfile(params.code_path), f"The path to the code is incorrect: {params.code_path}"
 
-    # Initialize translator
-    translator = Translator(params.model_path, params.BPE_path)
+    encoder = Encoder(params.model_path, params.BPE_path, )
 
-    # read input code from stdin
-    src_sent = []
-    input = sys.stdin.read().strip()
+    with open(params.code_path, "r", errors="ignore", encoding="utf8") as f:
+        input_ = f.read()
 
     with torch.no_grad():
-        output = translator.embed(input, lang=params.src_lang)
+        output = encoder.embed(input_)
     print(output)

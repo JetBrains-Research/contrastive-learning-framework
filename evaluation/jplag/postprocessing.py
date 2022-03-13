430ac4db38e61d320ffededc50cdec2176a5e525
maximzubkov <zubkov.md@phystech.edu>
2022-03-13 14:17:29 +0100

    Add C processing

--- evaluation/jplag/postprocessing.py
+++ evaluation/jplag/postprocessing.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 results_path = "/result"
 
 r = regex.compile(r'Comparing .*.(cpp|c)-.*.(cpp|c): ([0-9]*[.])?[0-9]+')
+r_c = regex.compile(r'Comparing .*.c-.*.c: ([0-9]*[.])?[0-9]+')
 
 
 def postprocess():
@@ -19,7 +20,10 @@ def postprocess():
     with open(jplag_results_path, 'r') as f_in, open(output_path, 'w') as f_out:
         for line in tqdm(f_in, total=total_lines):
             if r.match(line) is not None:
-                line_ = line[10:].replace(".cpp-", ".cpp,")
+                if r_c.match(line) is not None:
+                    line_ = line[10:].replace(".c-", ".c,")
+                else:
+                    line_ = line[10:].replace(".cpp-", ".cpp,")
                 similarity = float(line_[line_.index(sim_sep) + 2:])
                 if similarity:
                     line_ = line_.replace(": ", ",")

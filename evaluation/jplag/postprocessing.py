4e68d7a568fa7efbd1e5b87ffdc8c2c502121f86
maximzubkov <zubkov.md@phystech.edu>
2022-03-13 14:02:47 +0100

    Postprocessing script for jplag

---
+++ evaluation/jplag/postprocessing.py
@@ -0,0 +1,31 @@
+from os.path import join
+
+import regex
+from tqdm import tqdm
+
+data_path = "/data"
+results_path = "/result"
+
+r = regex.compile(r'Comparing .*.cpp-.*.cpp: ([0-9]*[.])?[0-9]+')
+
+
+def postprocess():
+    jplag_results_path = join(results_path, "results.txt")
+    output_path = join(results_path, "results.csv")
+    sim_sep = ": "
+
+    with open(jplag_results_path, 'r') as f:
+        total_lines = sum(1 for _ in f)
+
+    with open(jplag_results_path, 'r') as f_in, open(output_path, 'w') as f_out:
+        for line in tqdm(f_in, total=total_lines):
+            if r.match(line) is not None:
+                line_ = line[10:].replace(".cpp-", ".cpp,")
+                similarity = float(line_[line_.index(sim_sep) + 2:])
+                if similarity:
+                    line_ = line_.replace(": ", ",")
+                    f_out.write(line_ + "\n")
+
+
+if __name__ == "__main__":
+    postprocess()
